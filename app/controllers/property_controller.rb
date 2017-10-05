3c3fc06948e14ce90ae00cfdb22fad4ec6a9234f
Guilherme Zordan <guilhermezordan@outlook.com>
2017-10-05 19:09:13 -0300

    novo form

--- app/controllers/property_controller.rb
+++ app/controllers/property_controller.rb
@@ -3,4 +3,17 @@ class PropertyController < ApplicationController
     end
     def show
     end
+
+    def new_house
+    end
+
+    def new_apartment
+    end
+
+    def new_comercial
+    end
+
+    def new_terrain
+    end
+
 end
