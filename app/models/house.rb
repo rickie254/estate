38a2771c9d34c00ef80e02b5262d2e9ac831452b
Guilherme Zordan <guilhermezordan@outlook.com>
2017-10-18 20:12:51 -0200

    filtro de imoveis atraves de casas

--- app/models/house.rb
+++ app/models/house.rb
@@ -4,5 +4,19 @@ class House < Property
 	# validates :number_of_rooms, presence: true
 	# validates :profile, presence: true
 
+  def self.query_for_listing filter
+    if filter
+      if filter[:number_of_rooms]
+				if filter[:number_of_rooms] == 4
+					where("number_of_rooms >= 4")
+				else
+					where("number_of_rooms >= ?", filter[:number_of_rooms])
+				end
+      end
+    else
+      all
+    end
+  end
+
 
 end
