d7fb0422124b1e4d9a0203ac3bd45158271d50ad
Guilherme Zordan <guilhermezordan@outlook.com>
2017-10-27 14:46:06 -0200

    possibilidade de filtrar tipos de imoveis

--- app/models/terrain.rb
+++ app/models/terrain.rb
@@ -1,3 +1,12 @@
 class Terrain < Property
   enum terrain_kind: [ :rural, :farm, :residential, :comercial, :condominium ]
+
+  def self.query_for_listing filter
+    if filter && filter[:terrain_kind]
+      p filter
+      where(terrain_kind: filter[:terrain_kind])
+    else
+      all
+    end
+  end
 end
